09ef4437ba16315a50206c23e5e4c336b3c16772
asif10388 <asif.khan10388@gmail.com>
2023-04-30 21:31:03 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./comps/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello MY ASS NIGGA!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
